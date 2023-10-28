3714864b8c67f5831eca8aededd8bcef1fef41e7
Anjali211003 <119666988+Anjali211003@users.noreply.github.com>
2023-10-28 11:41:42 +0530

    Add files via upload

---
+++ BLOG-APP/utils/appErr.js
@@ -0,0 +1,9 @@
+const appErr = (message, statusCode) => {
+    let error = new Error(message);
+    error.stack = error.stack;
+    error.statusCode = statusCode ? statusCode : 500;
+    return error;
+  };
+  
+  module.exports = appErr;
+  
